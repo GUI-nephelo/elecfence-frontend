6716ffacd93a8ea26ec0b2e067b69414eedfe8e9
GUI_ <51920543+GUI-nephelo@users.noreply.github.com>
2023-09-28 09:36:30 +0000

    fix

--- app/api/get_jwt/route.js
+++ app/api/get_jwt/route.js
@@ -1,5 +1,6 @@
+import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
-export async function GET({req}){
-    return NextResponse.json(req.nextauth)
-}
+// export async function GET({req}){
+//     return NextResponse.json(getToken({req}))
+// }
