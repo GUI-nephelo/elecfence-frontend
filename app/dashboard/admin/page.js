72d98e07eac2c7f6599622b0f3cac03c44830cf3
GUI_nephelo <1736319094@qq.com>
2023-10-16 12:55:08 +0800

    修复预渲染bug

--- app/dashboard/admin/page.js
+++ app/dashboard/admin/page.js
@@ -5,7 +5,12 @@ import UserManagement from "./userMangement";
 export default async function adminPage({ searchParams }) {
     // console.log("hello admin")
     // const userList = await getUser()
-    const userList = (await search("shadow")).hits.hits.map(x => x._source)
+    var userList = []
+    try {
+        userList = (await search("shadow")).hits.hits.map(x => x._source)
+    } catch (e) {
+        console.log(e)
+    }
 
     return (
         <UserManagement initialList={userList} />
