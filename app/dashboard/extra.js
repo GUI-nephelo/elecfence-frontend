3348427faf614c261c0f29d35f91831f0aace883
GUI_nephelo <1736319094@qq.com>
2023-11-12 12:03:26 +0800

    添加时间筛选功能

--- app/dashboard/extra.js
+++ app/dashboard/extra.js
@@ -1,11 +1,15 @@
 "use client"
 import { dataSearchColOpt, levelCol } from "@/lib/config";
-import { Button, Checkbox, Col, Input, Row } from "antd";
+import { Button, Checkbox, Col, DatePicker, Row } from "antd";
 import Search from "antd/es/input/Search";
+import dayjs from "dayjs";
 import { revalidatePath } from "next/cache";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
+
+
+const { RangePicker } = DatePicker;
 
 function assign(obj, k, v) {
     obj[k] = v
@@ -22,29 +26,38 @@ export function ExtraComponent({ role }) {
 
     const [whichCol, setWhichCol] = useState(options.map(x => x.value))
     const [searchString, setSearchString] = useState("")
+    const [dateRange, setDateRange] = useState([null, null])
 
     useEffect(() => {
         if (!("filter" in cookies)) {
-            setFilter({ searchString, whichCol })
+            setFilter({ searchString, whichCol, dateRange })
         } else {// 将cookie中的参数放在页面上
             setWhichCol(cookies["filter"]["whichCol"])
             setSearchString(cookies["filter"]["searchString"])
+            setDateRange(cookies["filter"]["dateRange"])
             // console.log("filter 在cookie中")
         }
     }, [router])
 
-    const onSearch = async (t) => {
-        // console.log(t)
+    const onSearch = async t => {
+
         setFilter(assign(cookies["filter"], "searchString", t))
         setSearchString(t)
         // revalidatePath("/dashboard")
         router.refresh()
     }
 
-    const onChange = async (v) => {
+    const onColCheckBoxChange = async v => {
         // console.log(v)
         setFilter(assign(cookies["filter"], "whichCol", v))
         setWhichCol(v)
+    }
+
+    const onRangePickerChange = async v => {
+        const range = Array.isArray(v) ? v.map(x => (x == null ? x : x.$d.getTime())) : [null, null]
+        setFilter(assign(cookies["filter"], "dateRange", range))
+        setDateRange(Array.isArray(v) ? v : [null, null])
+        router.refresh()
     }
 
     return (
@@ -59,10 +72,13 @@ export function ExtraComponent({ role }) {
                         style={{ width: 250 }}
                     ></Search>
                 </Col>
-                <Col offset={3}>
-                    <Checkbox.Group options={options} value={whichCol} onChange={onChange} />
+                <Col offset={1}>
+                    <Checkbox.Group options={options} value={whichCol} onChange={onColCheckBoxChange} style={{ margin: "0 auto" }} />
                 </Col>
-                <Col offset={9}>
+                <Col offset={1}>
+                    <RangePicker onChange={onRangePickerChange} value={dateRange.map(x => (x != null ? dayjs(x) : null))} allowEmpty={[true, true]} showTime />
+                </Col>
+                <Col offset={1}>
                     <Button type="primary" href="/api/export">导出</Button>
                 </Col>
             </Row>
