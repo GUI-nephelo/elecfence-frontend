8bed546ed054bb5a4d70475da2962dfed7fec478
GUI_nephelo <1736319094@qq.com>
2024-01-29 21:35:03 +0800

    添加批量导入功能

--- app/dashboard/setBlackWhite/listView.js
+++ app/dashboard/setBlackWhite/listView.js
@@ -1,7 +1,57 @@
 "use client"
-import { Button, Col, Form, Input, List, Row, Space } from "antd";
+import { Button, Col, Form, Input, List, Row, Space, Upload, message } from "antd";
 import { useState } from "react";
 import { BlackWhiteAddAction, BlackWhiteDeleteAction } from "../actions";
+import { UploadOutlined } from '@ant-design/icons';
+import * as XLSX from 'xlsx';
+
+function FileImportForm({ name, label, handleAdd }) {
+
+
+    function xlsx2json(file, type = "buffer") {
+
+        const workbook = XLSX.read(file, { type: type });
+        // console.log(workbook)
+        const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+        // console.log(worksheet)
+        const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+
+        console.log(jsonData)
+        // 获取数据 处理类型
+        const data = jsonData.map(x => `${x[0]}`).filter(v => !isNaN(v))
+        console.log('JSON data:', data);
+        return data
+    }
+
+    const onUpload = async (file) =>
+        new Promise((res, rej) => {
+            const reader = new FileReader()
+            reader.onload = (e) => {
+                // console.log(e)
+                const data = xlsx2json(e.target.result)
+                if (data.length) {
+                    handleAdd({ [name]: data }, name).then(o => {
+                        message.success(`读取${label}成功`);
+                        res(false);
+                    }).catch((error) => {
+                        message.success(`读取${label}失败: ${error}`);
+                        rej("读取失败");
+                    })
+                } else {
+                    message.success(`读取${label}失败，无数据`)
+                    rej("无数据")
+                }
+            }
+            reader.readAsArrayBuffer(file)
+        })
+
+
+    return (
+        <Upload accept=".xlsx,.xls,.csv" beforeUpload={onUpload}>
+            <Button icon={<UploadOutlined />}>导入{label}</Button>
+        </Upload>
+    )
+}
 
 
 
@@ -14,7 +64,12 @@ function ListView({ name, label, handleAdd, dataSource, handleDelete }) {
             size="large"
             className="min-width-list"
             header={<h1>{label}:手机号</h1>}
-            footer={<InputNumForm {...{ name, label, handleAdd }} />}
+            footer={
+                <Row gutter={15}>
+                    <Col><InputNumForm {...{ name, label, handleAdd }} /></Col>
+                    <Col><FileImportForm {...{ name, label, handleAdd }} /></Col>
+                </Row>
+            }
             dataSource={dataSource}
             bordered
             renderItem={item => (
@@ -51,19 +106,24 @@ function InputNumForm({ name, label, handleAdd }) {
 }
 
 export function BlackWhiteListView({ initialList }) {
-    console.log(initialList)
+    // console.log(initialList)
 
     const [list, setList] = useState(/*{ whiteList: [], blackList: [] }*/initialList)
 
     const handleAdd = async (e, key) => {
-        if (list[key].includes(e[key])) return
+        //  e => {key: <value>}
+        var add_value = e[key] instanceof Array ? e[key] : [e[key]]
+
+        // 筛选有交集的数据
+        add_value = add_value.filter(v => { return !list[key].includes(v) })
+        if (!add_value) return
         setList(prevList => {
             return {
                 ...prevList,
-                [key]: [...prevList[key], e[key]]
+                [key]: [...prevList[key], ...add_value]
             };
         });
-        await BlackWhiteAddAction({ ...e, key });
+        await BlackWhiteAddAction({ [key]: add_value, key });
     };
     const handleDelete = async (value, key) => {
         // if (!(list[key].includes(blackList[key]))) return;
